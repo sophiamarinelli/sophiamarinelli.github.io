8f0a8898c593f0fe6ad162ffc4a98d277505392e
sophiamarinelli <31392946+sophiamarinelli@users.noreply.github.com>
2017-10-08 09:12:29 -0400

    Grid Exercise!

---
+++ creative-computing/grid_exercise/javascript.js
@@ -0,0 +1,107 @@
+document.write("<h2>1</h2>")
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (i < 5) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+document.write("<h2>2</h2>")
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (j < 5) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+document.write("<h2>3</h2>")
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (i==j) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+document.write("<h2>4</h2>")
+for (var i = 0; i < 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (i>=j) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+document.write("<h2>5</h2>")
+for (var i = 1; i <= 10; i = i + 1) {
+    for (var j = 0; j < 10; j = j + 1) {
+        console.log("Row " + i + ", Column " + j);
+
+        // The next line is the only JS you'll have to change.
+        if (i%2) {
+            console.log("   pink");
+            document.write("<div class='box pink'></div>");
+        } else {
+            console.log("   turquoise");
+            document.write("<div class='box turquoise'></div>");
+        }
+    }
+
+    document.write('<br />');
+}
+
+document.write('<br />');
+
+
+
+
+
+
+
+
---
+++ creative-computing/javascript_practice/javascript.js
@@ -0,0 +1,12 @@
+var animals = [
+	"dog",
+	"pig",
+	"fish", 
+	"cat"
+	]
+
+for (var i=0; i<animals.length; i=i+1) {
+		console.log(i)
+		console.log(animals[i])
+		console.log("<img src= " + animals[i] + ".jpg")
+}
